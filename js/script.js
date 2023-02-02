83a2ca870ddf25ec682e0b08494967589bd10324
AleksanderKasprzak <123496024+OloManolo95@users.noreply.github.com>
2023-02-02 18:04:26 +0100

    add score of the game

--- js/script.js
+++ js/script.js
@@ -38,3 +38,40 @@ if(playerInput == '2'){
   }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+ if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+ if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Mamy remis!');
+  }
+  if( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Ty przegrywasz!');
+  }
+  if( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Mamy remis!');
+  }
+  if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+  }
+  if( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Ty przegrywasz!');
+  }
+  if( computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Ty przegrywasz!');
+  }
+  if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Mamy remis!');
+  }
+  if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+  }
+  if( computerMove == 'nożyce' && playerMove == 'nieznany ruch'){
+    printMessage('Wybierz ruch! 1,2 lub 3.');
+  }
+  if( computerMove == 'papier' && playerMove == 'nieznany ruch'){
+    printMessage('Wybierz ruch! 1,2 lub 3.');
+  }
+  if( computerMove == 'kamień' && playerMove == 'nieznany ruch'){
+    printMessage('Wybierz ruch! 1,2 lub 3.');
+  }
